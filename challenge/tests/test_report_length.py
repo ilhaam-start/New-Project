3c7f2175d5e7395c86ab93c7afbc80e979370264
Ilhaam <ilhaam777@gmail.com>
2023-04-24 14:16:30 +0100

    first test

--- challenge/tests/test_report_length.py
+++ challenge/tests/test_report_length.py
@@ -1,4 +1,7 @@
 from lib.report_length import *
 
 # This tests for word "hello"
-def tests_
+def tests_hello_returns_five():
+    result = report_length("hello")
+    assert result == "This string was 5 characters long."
+
