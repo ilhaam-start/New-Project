d29a58d492aab63b8301eb8cfe2d45f776eea083
Ilhaam <ilhaam777@gmail.com>
2023-04-26 11:13:31 +0100

    Completed challenge for count words

--- challenges/tests/test_count_words.py
+++ challenges/tests/test_count_words.py
@@ -4,14 +4,19 @@ from lib.count_words import *
 #This tests an empty string
 def test_empty_string_returns_string():
     result = count_words("")
-    assert result == ""
+    assert result == 0
 
 #This tests the length of the string.
 def test_length_of_string_returns_length():
     result = count_words("This is a string")
     assert result == 4
 
-#This tests if its not a string and returns the item.
-def test_if_not_string_return_item():
-    result = count_words(66)
-    assert result == 66
+#This tests a string with special characters
+def test_string_with_special_characters_returns_length():
+    result = count_words("This is @ a string !")
+    assert result == 6
+
+#This tests for one long string
+def test_a_string_all_together():
+    result = count_words("Thisisalongstring")
+    assert result == 1
