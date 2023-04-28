acd63c2e3a1858b1a4fc7f6982caa57253df8344
Ilhaam <ilhaam777@gmail.com>
2023-04-28 08:33:48 +0100

    "Diary entry working correctly"

--- testdrive_single_classes/diaryentry.py
+++ testdrive_single_classes/diaryentry.py
@@ -1,68 +1,45 @@
 import math
 class DiaryEntry():
     def __init__(self, title, contents):
-        # Parameters:
-        #   title: string
-        #   contents: string
+
         self.title = title
         self.contents = contents
         self.words_read = 0
 
 
     def format(self):
-        # Returns:
-        #   A formatted diary entry, for example:
-        #   "My Title: These are the contents"
+
         formatted = f"{self.title}: {self.contents}"
         return formatted
 
     def count_words(self):
         return len(self.title.split()) + len(self.contents.split())
-        # Returns:
-        #   int: the number of words in the diary entry
-
 
     def reading_time(self, wpm):
-        # Parameters:
-        #   wpm: an integer representing the number of words the user can read 
-        #        per minute
-        # Returns:
-        #   int: an estimate of the reading time in minutes for the contents at
-        #        the given wpm.
+        
         if self.count_words() == 2:
             return  1
         else:
             return math.ceil(self.count_words() / wpm)
 
     def reading_chunk(self, wpm, minutes):
-        # Parameters
-        #   wpm: an integer representing the number of words the user can read
-        #        per minute
-        #   minutes: an integer representing the number of minutes the user has
-        #            to read
-        # Returns:
-        #   string: a chunk of the contents that the user could read in the
-        #           given number of minutes
-        #
-        # If called again, `reading_chunk` should return the next chunk,
-        # skipping what has already been read, until the contents is fully read.
-        # The next call after that should restart from the beginning.
+        
         no_of_words = wpm * minutes
         string_list = self.format().split()
-        words_available = self.count_words()
+        # words_available = self.count_words() - TURNS OUT WE DON'T NEED THIS
 
-        if self.words_read > words_available:
+        if self.words_read >= len(string_list): #This is saying if the words read is greater than or equal to the length of our string list (so basically the words available) then bring the words ready back to zero.
             self.words_read = 0
 
-        start_chunk = self.words_read
-        end_chunk = no_of_words
-        reading_chunks = string_list[start_chunk:end_chunk]
-        print(f"Before it update, words read is: {self.words_read}")
-        self.words_read += end_chunk
-        print(f"After it update, words read is: {self.words_read}")
+        start_chunk = self.words_read #This is where we start reading
+        end_chunk = self.words_read + no_of_words # We end the chunk by adding the number of words to the amount read
+        reading_chunks = string_list[start_chunk:end_chunk] #This counts from the start chunk to the end chunk.
+        print(f"Before it updates, words read is: {self.words_read}")
+        self.words_read = end_chunk #Now the words read is equal to the end chunk, so that when we go back to the beginning it will say words read is now increased. (We didn't need to do self.words_read += end_chunk)
+        print(f"After it updates, words read is: {self.words_read}")
         print(no_of_words)
-        words_available -= self.words_read
-        return reading_chunks
+        # words_available -= self.words_read
+        return " ".join(reading_chunks) #Join the words so its not a list.
     
 
 
