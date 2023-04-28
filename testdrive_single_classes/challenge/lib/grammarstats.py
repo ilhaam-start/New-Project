94ff97433faba14cc63a9fc560ff6f9fdaa3de6e
Ilhaam <ilhaam777@gmail.com>
2023-04-28 12:08:53 +0100

    "recent commit"

--- testdrive_single_classes/challenge/lib/grammarstats.py
+++ testdrive_single_classes/challenge/lib/grammarstats.py
@@ -1,14 +1,14 @@
 class GrammarStats:
     def __init__(self):
-        self.text_checked = 0
-        self.checks_passed = 0
+        self.text_checked = 0 #Initialzed this
+        self.checks_passed = 0 #Initialzed this
 
     def check(self, text):
         if text == "":
             raise Exception("There is no text, cannot check grammar.")
-        self.text_checked += 1
+        self.text_checked += 1 #Adds one to the count of text checked
         if text[0].isupper() and text[-1] in ".!?":
-            self.checks_passed += 1
+            self.checks_passed += 1 #Adds one to the count of text which passed
             return True
         else:
             return False
@@ -20,4 +20,4 @@ class GrammarStats:
         if self.text_checked == 0:
             return 0
         else:
-            return int((self.checks_passed / self.text_checked) * 100)
+            return int((self.checks_passed / self.text_checked) * 100) #multiply by 100 to turn into percentage
