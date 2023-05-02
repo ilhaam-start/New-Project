866433bae60a37a0eadb2891e23d0ae87772c1ca
Ilhaam <ilhaam777@gmail.com>
2023-05-02 10:39:48 +0100

    "understood vowel remover debug"

--- debugging 2/tests/test_vowel_remover.py
+++ debugging 2/tests/test_vowel_remover.py
@@ -9,3 +9,9 @@ def test_long_sentence_with_punctuation():
     remover = VowelRemover("We will remove the vowels from this sentence.")
     result_no_vowels = remover.remove_vowels()
     assert result_no_vowels == "W wll rmv th vwls frm ths sntnc."
+
+#Test to check that the program can remove all the vowels from "aeiou", returning an empty string, "". 
+def test_all_vowels_return_empty_string():
+    remover = VowelRemover("aeiou")
+    result_no_vowels = remover.remove_vowels()
+    assert result_no_vowels == ""
