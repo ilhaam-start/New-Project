3933014373aed70b203a04b0ac558e08282039a4
Ilhaam <ilhaam777@gmail.com>
2023-05-04 15:34:26 +0100

    "completed"

--- mocking_bites/unit_testing_parent_child_classes/tests/test_music_library.py
+++ mocking_bites/unit_testing_parent_child_classes/tests/test_music_library.py
@@ -21,21 +21,26 @@ def test_keyword_return_matching_track():
     musiclibrary.add(fake_non_match)
     assert musiclibrary.search("hello") == [fake_match]
 
+#If a single track is added and keyword searched, return matching track
+def test_searches_by_keyword_with_single_tracks():
+    musiclibrary = MusicLibrary()
+    single_track = Mock()
+    single_track.matches.return_value == True
+    musiclibrary.add(single_track)
+    assert musiclibrary.search("my song") == [single_track]
 
+#initially music_library is empty
+def test_empty_tracks():
+    musiclibrary = MusicLibrary()
+    assert musiclibrary.music_library == []
 
-#If multiple tracks added and keyword search, return matching track
-# def test_keyword_return_matching_track():
-#     musiclibrary = MusicLibrary()
-#     fake_match = FakeMatchingTrack
-#     musiclibrary.add(FakeMatchingTrack)
-#     fake_non_match = FakeNotMatchingTrack
-#     musiclibrary.add(FakeNotMatchingTrack)
-#     assert musiclibrary.search("hello") == [fake_match]
-
-# class FakeMatchingTrack():
-#     def matches(self, keyword):
-#         return True
-
-# class FakeNotMatchingTrack():
-#     def matches(self, keyword):
-#         return False
+#when I add some tracks they show up in the list
+def test_tracks_added_appear_in_list():
+    musiclibrary = MusicLibrary()
+    track_1 = Mock()
+    track_2 = Mock()
+    track_3 = Mock()
+    musiclibrary.add(track_1)
+    musiclibrary.add(track_2)
+    musiclibrary.add(track_3)
+    assert musiclibrary.music_library == [track_1, track_2, track_3]
